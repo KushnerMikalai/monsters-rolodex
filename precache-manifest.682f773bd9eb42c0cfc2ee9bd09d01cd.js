b06cc4d9329a8f6b8ec0cd152b4080389ae4d221
KushnerMikalai <nickolaikushner@gmail.com>
2019-08-12 22:33:29 +0300

    Updates

---
+++ precache-manifest.682f773bd9eb42c0cfc2ee9bd09d01cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2076bc728484cb21af797b9c0ef38268",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "26c2bf6b2e314a1fb0ed",
+    "url": "/monsters-rolodex/static/css/main.5a5d5abb.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/monsters-rolodex/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "26c2bf6b2e314a1fb0ed",
+    "url": "/monsters-rolodex/static/js/main.fc183772.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
